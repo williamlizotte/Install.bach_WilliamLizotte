1791f659df5f41f200331052575ef34a77f3f46a
WilliamLizotte <61836@cstjean.qc.ca>
2025-08-28 14:14:49 +0000

    deuxieme

---
+++ Devoir_2_progSys.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir bin
+mkdir cegep
+mkdir projets
+sudo pacman -S git vim gcc gdb python3 python3-pip python-venv
+alias ls = 'chk'
+alias ll = 'chks'
+alias clear = 'c'
+alias grep = 'grepc --color=auto'
+
+
+
