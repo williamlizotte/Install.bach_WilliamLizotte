5fbc2e41f33e099f10044effc00bcdb3f52097ea
WilliamLizotte <61836@cstjean.qc.ca>
2025-09-02 13:09:26 +0000

    Mon devoir

--- Devoir_2_progSys.sh
+++ Devoir_2_progSys.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "$USER" 
 mkdir bin
 mkdir cegep
 mkdir projets
@@ -6,7 +7,16 @@ sudo pacman -S git vim gcc gdb python3 python3-pip python-venv
 alias ls = 'chk'
 alias ll = 'chks'
 alias clear = 'c'
-alias grep = 'grepc --color=auto'
+alias grepc = 'grep --color=auto'
 
+function OuvrirFichier{
+mkdir FichierFunction 
+nvim FichierFunction
+} 
+
+if[/u = "etudiant"]
+then
+	PS1 = "etdudiant"
+fi
 
 
